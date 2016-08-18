cf520122a9c35c78b0784efd87172f92e6350344
deef <deef551@gmail.com>
2016-08-18 00:59:56 -0500

    add output for POJO Stream objects from a username in GetLiveNames.

--- src/main/java/tech/deef/twitch/manipulation/GetLiveNames.java
+++ src/main/java/tech/deef/twitch/manipulation/GetLiveNames.java
@@ -1,18 +1,33 @@
 package tech.deef.twitch.manipulation;
 
+import java.util.ArrayList;
+
+import tech.deef.twitch.domain.Stream;
 import tech.deef.twitch.external.DataPull;
 import tech.deef.twitch.external.DataPuller;
 import tech.deef.twitch.external.TwitchAPI;
 import tech.deef.twitch.external.TwitchAPIPull;
 
 public class GetLiveNames {
+	//Returns an array of names that are all of the live users followed by username
 	public static String[] getLiveNames(String username){
 		DataPull pull = new DataPuller();
 		TwitchAPI puller = new TwitchAPIPull(pull);
 		GetStreams pulling = new GetStreams(puller);
 		String[] liveNames = null;
-		liveNames = pulling.getLiveStreams(GetFollowed.getFollowed(puller.getUserFollowsChannels(username)));
+		liveNames = pulling.getLiveStreamsNames(GetFollowed.getFollowed(puller.getUserFollowsChannels(username)));
 		System.out.println("INFO: The Number of people live: " + liveNames.length);
+		return liveNames;
+	}
+	
+	//returns an arraylist of Stream POJOs of all live users followed by username
+	public static ArrayList<Stream> getLiveStreams(String username){
+		DataPull pull = new DataPuller();
+		TwitchAPI puller = new TwitchAPIPull(pull);
+		GetStreams pulling = new GetStreams(puller);
+		ArrayList<Stream> liveNames = new ArrayList<Stream>();
+		liveNames = pulling.getLiveStreams(GetFollowed.getFollowed(puller.getUserFollowsChannels(username)));
+		System.out.println("INFO: The Number of people live: " + liveNames.size());
 		return liveNames;
 	}
 }
--- src/main/java/tech/deef/twitch/manipulation/GetStreams.java
+++ src/main/java/tech/deef/twitch/manipulation/GetStreams.java
@@ -2,6 +2,7 @@ package tech.deef.twitch.manipulation;
 
 import java.util.ArrayList;
 
+import tech.deef.twitch.domain.Stream;
 import tech.deef.twitch.domain.StreamsUser;
 import tech.deef.twitch.external.TwitchAPI;
 
@@ -13,24 +14,44 @@ public class GetStreams {
 		this.puller = puller;
 	}
 
-	public String[] getLiveStreams(String[] streams) {
+	public String[] getLiveStreamsNames(String[] streams) {
 		StreamsUser user = null;
 		ArrayList<String> liveUsers = new ArrayList();
 		for (String testUser : streams) {
 			// System.out.print("\n"+testUser);
 			user = puller.getStreamsUser(testUser);
-			if(user != null){if (user.getStream() != null) {
-				liveUsers.add(testUser);
-			}}else{
+			if (user != null) {
+				if (user.getStream() != null) {
+					liveUsers.add(testUser);
+				}
+			} else {
 				System.out.println("ERROR: User is null in getLiveStreams. User: " + testUser);
 			}
 		}
 
 		String[] live = new String[liveUsers.size()];
-		for(int i = 0; i < liveUsers.size(); i++){
+		for (int i = 0; i < liveUsers.size(); i++) {
 			live[i] = liveUsers.get(i);
 		}
-		
+
 		return live;
+	}
+
+	public ArrayList<Stream> getLiveStreams(String[] streams) {
+		StreamsUser user = null;
+		ArrayList<Stream> liveUsers = new ArrayList();
+		for (String testUser : streams) {
+			// System.out.print("\n"+testUser);
+			user = puller.getStreamsUser(testUser);
+			if (user != null) {
+				if (user.getStream() != null) {
+					liveUsers.add(user.getStream());
+				}
+			} else {
+				System.out.println("ERROR: User is null in getLiveStreams. User: " + testUser);
+			}
+		}
+
+		return liveUsers;
 	}
 }
