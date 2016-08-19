d9ad8e54feca3f9c83bb085b5c795720c7fe8eb0
deef <deef551@gmail.com>
2016-08-19 00:58:48 -0500

    add auto-updating and recent live emphisis
    streams live within an hour will be larger and green.

--- src/main/java/tech/deef/twitch/server/FancyServer.java
+++ src/main/java/tech/deef/twitch/server/FancyServer.java
@@ -24,51 +24,70 @@ import tech.deef.twitch.manipulation.GetLiveNames;
 @WebServlet("/FancyTwitch/*")
 public class FancyServer extends HttpServlet {
 
+	private final int RELOAD_TIME = 60000;
+	private final int RELOAD_FAIL_TIMEOUT = 10000;
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		ServletWorker.stringOutput(this.getClass(), request, response, (String username, PrintWriter out) -> {
 			List<Stream> liveNames = GetLiveNames.getLiveStreams(username);
 
 			Collections.sort(liveNames, new Comparator<Stream>() {
-		        @Override
-		        public int compare(Stream stream2, Stream stream1)
-		        {
+				@Override
+				public int compare(Stream stream2, Stream stream1) {
+					return stream2.getChannel().getDisplayName().toLowerCase()
+							.compareTo(stream1.getChannel().getDisplayName().toLowerCase());
+				}
+			});
 
-		            return  stream2.getChannel().getDisplayName().toLowerCase().compareTo(stream1.getChannel().getDisplayName().toLowerCase());
-		        }
-		    });
-			
 			StringBuffer buffer = new StringBuffer();
 
 			buffer.append("<h3>The following people " + username + " Follows are live</h3>");
 			buffer.append("<p>");
 			for (Stream stream : liveNames) {
-				buffer.append("<a href=\"https://www.twitch.tv/" + stream.getChannel().getDisplayName() + "\">" + stream.getChannel().getDisplayName() + "</a>");
-				buffer.append(" - " + stream.getChannel().getGame() + "<br /> " + stream.getChannel().getStatus() + "<br />");
 				String time = stream.getCreatedAt();
-				SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-				Date now = new Date();
 				try {
-					Date then = dateFormat.parse(time);
-					Duration liveTime = Duration.ofMillis((now.getTime()-then.getTime())).abs();
-					buffer.append("<font size=\"2\" color=\"red\">Live for: " +""+ liveTime.toDays() + "d " 
-					+ liveTime.toHours()%24 + ":" + liveTime.toMinutes()%60 + ":" + liveTime.getSeconds()%60 + "<br />");
+					SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+					Date now = new Date();
+					Date then;
 
+					then = dateFormat.parse(time);
+					if (Duration.ofMillis((now.getTime() - then.getTime())).abs().getSeconds() < 3600) {
+						buffer.append("<font size=\"5\" color=\"green\">");
+					}
+
+					buffer.append("<a href=\"https://www.twitch.tv/" + stream.getChannel().getDisplayName() + "\">"
+							+ stream.getChannel().getDisplayName() + "</a>");
+					buffer.append(" - " + stream.getChannel().getGame() + "<br /> " + stream.getChannel().getStatus()
+							+ "<br />");
+
+					Duration liveTime = Duration.ofMillis((now.getTime() - then.getTime())).abs();
+					if (Duration.ofMillis((now.getTime() - then.getTime())).abs().getSeconds() < 3600) {
+						buffer.append("Live for: " + "" + liveTime.toDays() + "d " + liveTime.toHours() % 24 + ":"
+								+ liveTime.toMinutes() % 60 + ":" + liveTime.getSeconds() % 60 + "<br />");
+					}else
+					{
+						buffer.append("<font size=\"2\" color=\"red\">Live for: " + "" + liveTime.toDays() + "d "
+								+ liveTime.toHours() % 24 + ":" + liveTime.toMinutes() % 60 + ":"
+								+ liveTime.getSeconds() % 60 + "<br />");
+					}
+
+					buffer.append("Viewers: " + stream.getViewers() + "</font>");
+
+					buffer.append("<br /><br />");
 				} catch (ParseException e) {
 					// TODO Auto-generated catch block
-					System.out.println("Time parse failure. Time: " + time);
+					System.out.println("ERROR: Time parse failure. Time: " + time);
 					e.printStackTrace();
+					;
 				}
-				buffer.append("Viewers: " + stream.getViewers() + "</font>");
-				buffer.append("<br /><br />");
 			}
 			buffer.append("</p>");
-			
-			buffer.append("<script>setTimeout(function(){window.location.reload(true);setTimeout(function(){alert(\"ReloadFailed\");},10000)},(1000*3))</script>");
-			
-			
+			buffer.append("<script>setTimeout(function(){"
+					+ "window.location.reload(true);setTimeout(function(){alert(\"ReloadFailed\");},"
+					+ RELOAD_FAIL_TIMEOUT + ")},(" + RELOAD_TIME + "))</script>");
 			out.write(buffer.toString());
-			
+
 		});
 	}
 
